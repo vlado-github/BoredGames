e7af1ccd02164cf7743d91053502c8ad931f999c
Vladimir Marinkovic <vladimir@keystone.no>
2023-03-31 13:29:56 +0200

    grains added

---
+++ BoredGames.Server/BoredGames.Server.Common/Enums/GameState.cs
@@ -0,0 +1,9 @@
+﻿namespace BoredGames.Server.Common.Enums;
+
+[Serializable]
+public enum GameState
+{
+    AwaitingPlayers,
+    InPlay,
+    Finished
+}
--- BoredGames.Server/BoredGames.Server.Domain/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BoredGames.Server.Domain;
-
-public class Class1
-{
-}
