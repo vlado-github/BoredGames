8e3422adbcf214d1da2c4a817eba1ec5ede9d1ce
Vladimir Marinkovic <marinkovicvlado@gmail.com>
2023-07-29 18:43:26 +0200

    bugfix gamescore response

--- BoredGames.Server/BoredGames.Server.Service/ViewModels/GameScoreViewModel.cs
+++ BoredGames.Server/BoredGames.Server.Service/ViewModels/GameScoreViewModel.cs
@@ -5,7 +5,10 @@ namespace BoredGames.Server.Service.ViewModels;
 [GenerateSerializer]
 public class GameScoreViewModel
 {
+    [Id(0)]
     public int CurrentRound { get; set; }
+    [Id(1)]
     public int RequiredNumberOfWins { get; set; }
+    [Id(2)]
     public IList<PlayerScoreViewModel> PlayerScores { get; set; }
 }
