a987241e825cf381eb28dfdaee91d3188cb667c0
Vladimir Marinkovic <marinkovicvlado@gmail.com>
2024-08-11 14:56:53 +0200

    removed argon

--- BoredGames.Client/BoredGames.Portal/src/main.js
+++ BoredGames.Client/BoredGames.Portal/src/main.js
@@ -4,14 +4,12 @@ import router from './router'
 import Toast from "vue-toastification"
 import { plugin, defaultConfig } from '@formkit/vue'
 import formkitConfig from './formkit.config.js'
-import Argon from '@/plugins/argon-kit'
 
 
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App)
 
-app.use(Argon);
 app.use(router)
 app.use(Toast)
 app.use(plugin, defaultConfig(formkitConfig))
