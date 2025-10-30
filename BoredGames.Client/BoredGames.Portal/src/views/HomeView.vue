044501fc11f000b5419e07078a9d3ffeab717763
Vladimir Marinkovic <marinkovicvlado@gmail.com>
2025-10-30 13:35:50 +0100

    remove vue.js clashofhands implementation

--- BoredGames.Client/BoredGames.Portal/src/views/HomeView.vue
+++ BoredGames.Client/BoredGames.Portal/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
 import GameGallery from '../components/GameGallery.vue'
-import GamePlayer from '@/components/GamePlayer.vue';
 </script>
 
 <template>
    <GameGallery />
-   <!-- <GamePlayer gameTitle="clash-of-hands"/> -->
 </template>
